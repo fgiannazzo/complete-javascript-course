7efec2e2e96d7cbd6b30c261516cbf07c7c65c22
fgiannazzo <felipe@v1now.com>
2020-03-27 12:21:28 -0400

    new commit

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,3 +1,87 @@
+// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+
+console.log(years);
+console.log(ages);
+console.log(fullAges);
+
+/* Primitives vs Objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+}
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+
+*/
+/* Obect.create
+
+var personeProto = {
+    calculateAge: function() {
+        console.log(2020 - this.yearOfBirth);
+    }
+}
+
+var john = Object.create(personeProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personeProto, 
+    {
+        name: { value: 'Jane' },
+        yearOfBirth: {value: 1969 },
+        job: { value: 'designer' } 
+    }); 
+*/
+/* Function Constructors
 // Function constructor
 
 var Person = function (name, yearOfBith, job) {
@@ -21,3 +105,4 @@ john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
 
+*/
